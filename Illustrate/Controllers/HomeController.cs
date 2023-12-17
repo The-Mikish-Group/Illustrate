66add5acf4341a20810dd7fbac3b4058cdf8443d
Michael Vaughn <mikish.vaughn@gmail.com>
2023-12-16 21:46:06 -0600

    Update dynamic controller and menu stuff to read new folders in auomatically.

--- Illustrate/Controllers/HomeController.cs
+++ Illustrate/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace Illustrate.Controllers
 {
     public class HomeController : Controller
     {
-         private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -18,59 +18,23 @@ namespace Illustrate.Controllers
             return View();
         }
 
-        public IActionResult Flowers()
+        public IActionResult Images(string viewName)
         {
-            string viewName = ControllerContext.ActionDescriptor.ActionName;
             ViewData["ViewName"] = viewName;
-
-            return View("Images");
-        }
-
-        public IActionResult Seashore()
-        {
-            string viewName = ControllerContext.ActionDescriptor.ActionName;
-            ViewData["ViewName"] = viewName;
-
-            return View("Images");
-        }
-
-        public IActionResult Religious()
-        {
-            string viewName = ControllerContext.ActionDescriptor.ActionName;
-            ViewData["ViewName"] = viewName;
-
-            return View("Images");
-        }
-
-        public IActionResult Misc()
-        {
-            string viewName = ControllerContext.ActionDescriptor.ActionName;
-            ViewData["ViewName"] = viewName;
-
-            return View("Images");
-        }
-
-        public IActionResult Christmas()
-        {
-            string viewName = ControllerContext.ActionDescriptor.ActionName;
-            ViewData["ViewName"] = viewName;
-
-            return View("Images");
+            return View();
         }
 
         public IActionResult Index()
         {
             string viewName = ControllerContext.ActionDescriptor.ActionName;
             ViewData["ViewName"] = viewName;
-
             return View();
-        } 
+        }
 
         public IActionResult Contact()
         {
             string viewName = ControllerContext.ActionDescriptor.ActionName;
             ViewData["ViewName"] = viewName;
-
             return View();
         }
 
@@ -78,7 +42,6 @@ namespace Illustrate.Controllers
         {
             string viewName = ControllerContext.ActionDescriptor.ActionName;
             ViewData["ViewName"] = viewName;
-
             return View();
         }
 
@@ -86,11 +49,8 @@ namespace Illustrate.Controllers
         {
             string viewName = ControllerContext.ActionDescriptor.ActionName;
             ViewData["ViewName"] = viewName;
-
             return View();
         }
-       
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
