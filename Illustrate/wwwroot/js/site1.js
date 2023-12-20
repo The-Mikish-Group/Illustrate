fd56e91dae016d4c75c6cc09006f732e020381e6
Michael Vaughn <mikish.vaughn@gmail.com>
2023-12-19 21:24:08 -0600

    Gallery Update

--- Illustrate/wwwroot/js/site1.js
+++ Illustrate/wwwroot/js/site1.js
@@ -1,8 +1,7 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
+    var images = document.querySelectorAll("img[data-src]");
     var imageContainer = document.getElementById("image-container");
     var currentRequest;
-    var baseUrl = '/home/images';
-
     var observer = new IntersectionObserver(
         function (entries, observer) {
             entries.forEach(function (entry) {
@@ -15,8 +14,11 @@
         }
     );
 
+    images.forEach(function (img) {
+        observer.observe(img);
+    });
+
     var checkLoadingCompletion = function () {
-        var images = document.querySelectorAll("img[data-src]");
         if (Array.from(images).every(img => img.complete)) {
             imageContainer.style.display = "flex";
         } else {
@@ -26,7 +28,6 @@
 
     checkLoadingCompletion();
 
-    // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function () { scrollFunction() };
 
     function scrollFunction() {
@@ -38,93 +39,62 @@
         }
     }
 
-    // When the user clicks on the button, scroll to the top of the document
     window.topFunction = function () { topFunction() };
 
     function topFunction() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     }
+});
 
-    // Event listener for menu links
-    document.querySelectorAll('.menu-link').forEach(function (link) {
-        link.addEventListener('click', function () {
-            var viewName = link.dataset.viewName;
+document.querySelectorAll('.menu-link').forEach(function (link) {
+    link.addEventListener('click', function () {
+        var viewName = link.dataset.viewName;
 
-            if (currentRequest) {
-                currentRequest.abort();
-            }
+        if (currentRequest) {
+            currentRequest.abort();
+        }
 
-            // Display a loading spinner or any other visual indication
-            imageContainer.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
+        imageContainer.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
 
-            // Make a new request for the selected view with a delay
-            setTimeout(function () {
-                fetchData(viewName);
-            }, 4000); // Adjust the delay time (in milliseconds) as needed
-        });
+        fetchData(viewName);
+    });
+});
+
+var baseUrl = '/home/images';
+
+function fetchData(viewName) {
+    if (viewName) {
+        currentRequest = fetch(`${baseUrl}?viewName=${viewName}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.text();
+            })
+            .then(html => updateUI(html, viewName))  // Assuming you have an updateUI function
+            .catch(error => handleError(error, viewName));
+    } else {
+        window.location.href = '/home/Images?viewName=flowers'; // Adjust the URL as needed
+    }
+}
+
+function updateUI(html, viewName) {
+    var newImages = document.querySelectorAll("#image-container img[data-src]");
+    newImages.forEach(function (img) {
+        observer.observe(img);
     });
 
-    // Function to fetch data
-    function fetchData(viewName) {
-        if (viewName) {
-            // Make a request to the server using fetch
-            fetch(`${baseUrl}?viewName=${viewName}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! Status: ${response.status}`);
-                    }
-                    return response.text();
-                })
-                .then(html => updateUI(html, viewName))
-                .catch(error => handleError(error, viewName));
-        } else {
-            // Handle undefined viewName, for example, redirect to the home page
-            window.location.href = '/home/Images?viewName=flowers'; // Adjust the URL as needed
-        }
-    }
+    checkLoadingCompletion();
+    hideCurrentMenuSelection(viewName);
+}
 
-    // Function to update UI
-    function updateUI(html, viewName) {
-        // Update the UI with the fetched HTML content
-        // Example: imageContainer.innerHTML = html;
+function handleError(error, viewName) {
+    console.error('Error loading view:', error);
 
-        // After updating the UI, you can trigger lazy loading or perform other actions
-        // For example, you might need to observe new images if they are added dynamically
-        var newImages = document.querySelectorAll("#image-container img[data-src]");
-        newImages.forEach(function (img) {
-            observer.observe(img);
-        });
+    imageContainer.innerHTML = '<div class="alert alert-danger" role="alert">Oops! Something went wrong. Redirecting to the home page...</div>';
 
-        // Check loading completion if necessary
-        checkLoadingCompletion();
-
-        // Hide the current menu selection
-        hideCurrentMenuSelection(viewName);
-    }
-
-    // Function to handle errors
-    function handleError(error, viewName) {
-        console.error('Error loading view:', error);
-
-        // Display a user-friendly error message
-        imageContainer.innerHTML = '<div class="alert alert-danger" role="alert">Oops! Something went wrong. Redirecting to the home page...</div>';
-
-        // Redirect to the home page after a delay (e.g., 3 seconds)
-        setTimeout(function () {
-            window.location.href = '/'; // Update the URL as needed
-        }, 4000);
-    }
-
-    // Function to hide current menu selection
-    function hideCurrentMenuSelection(viewName) {
-        var elements = document.querySelectorAll('.menu-item');
-        elements.forEach(function (element) {
-            if (element.getAttribute('data-viewName') === viewName) {
-                element.style.display = 'none';
-            } else {
-                element.style.display = '';
-            }
-        });
-    }
-});
+    setTimeout(function () {
+        window.location.href = '/'; // Update the URL as needed
+    }, 4000);
+}
